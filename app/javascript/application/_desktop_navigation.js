eb3d028b09f795a0d0432542d499176a0fd705cd
abhchand <me@abhchand.me>
2019-09-05 20:57:58 -0400

    Restructuring desktop and mobile navigation to be left-fixed column

--- app/javascript/application/_desktop_navigation.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $(".desktop-navigation").on("click", ".desktop-navigation__toggle", function(e) {
-    e.preventDefault();
-
-    const nav = $(".desktop-navigation");
-    $(nav).toggleClass("desktop-navigation--collapsed");
-  });
-});
