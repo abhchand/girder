b65d3c9dfe1cc5a3d93690d7ca7c3391c9176df1
abhchand <me@abhchand.me>
2019-12-09 19:24:10 -0500

    Integrating Webpack into the Rails application
    * Adding Webpack helper functions to include and serve assets
    * Removing old Sprockets JS and CSS application manifest files
    * Adding new Webpack JS and CSS application packs
    * Disabling Asset pipeline. Disabling individual commits will be a separate commit.
    * Updating foreman `webpack` command to use `webpack-dev-server`
    * `inline_svg` gem:
     * Updating `inline_svg` gem used to serve SVG assets.
     * Specifying a customer asset loader.
     * Updating `inline_svg` call to `inline_svg_tag`

--- app/frontend/javascript/packs/application.js
+++ app/frontend/javascript/packs/application.js
@@ -1,16 +1,15 @@
 /* eslint no-console:0 */
 
 // Expose JQuery globally
-window.jQuery = require('jquery');
-window.$ = window.jQuery;
+window.$ = window.jQuery = require('jquery');
 require('jquery-ujs');
 
 import 'core-js/stable';
 
-import 'mount-react-component';
+import 'javascript/mount-react-component';
 
-import 'application/_mobile_navigation.js';
-import 'application/_modal.js';
-import 'application/flash.js';
+import 'javascript/application/_mobile_navigation.js';
+import 'javascript/application/_modal.js';
+import 'javascript/application/flash.js';
 
-import 'components/example';
+import 'javascript/components/example';
