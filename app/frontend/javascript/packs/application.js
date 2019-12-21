9f56a7e1634978633d907c00ee4578437fbaba97
abhchand <me@abhchand.me>
2019-12-20 21:53:37 -0500

    Migrating to using React Modal implementation
    This implements a pure React modal that we consistently use for
    
    * Product Feedback
    * Collections Delete (index + show pages)
    * Collections Sharing (index + show pages)
    
    This commit includes the Modal implementation as well as migrating the
    above components. It also includes refactoring of spec helpers needed to
    streamline the specs

--- app/frontend/javascript/packs/application.js
+++ app/frontend/javascript/packs/application.js
@@ -8,10 +8,12 @@ import 'core-js/stable';
 
 import 'javascript/mount-react-component';
 
-import 'javascript/application/_desktop_navigation.js';
-import 'javascript/application/_mobile_navigation.js';
-import 'javascript/application/_modal.js';
+import 'javascript/application/_desktop_navigation';
+import 'javascript/application/_mobile_navigation';
+import 'javascript/application/_global_modal_close';
 import 'javascript/application/flash.js';
 
 import 'javascript/components/example';
+import 'javascript/components/icons';
+import 'javascript/components/modal';
 import 'javascript/components/product_feedback_form';
