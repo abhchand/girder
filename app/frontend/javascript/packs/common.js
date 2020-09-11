177117fb04b9c7b06db6176a5388454c3bec3bdc
abhchand <me@abhchand.me>
2020-09-10 22:14:25 -0400

    Use smaller and page-specific packs for JS and CSS

--- app/frontend/javascript/packs/common.js
+++ app/frontend/javascript/packs/common.js
@@ -6,14 +6,12 @@ require('jquery-ujs');
 
 import 'core-js/stable';
 
-import 'javascript/mount-react-component';
-
 import 'javascript/application/_desktop_navigation';
-import 'javascript/application/_mobile_navigation';
 import 'javascript/application/_global_modal_close';
+import 'javascript/application/_mobile_navigation';
 import 'javascript/application/flash.js';
 
 import 'javascript/components/example';
-import 'javascript/components/icons';
-import 'javascript/components/modal';
 import 'javascript/components/product_feedback_form';
+
+import 'javascript/mount-react-component';
