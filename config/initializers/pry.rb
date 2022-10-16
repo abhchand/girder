df154ce42f78f3b242dd453d43b16d988c5a5847
abhchand <me@abhchand.me>
2022-10-15 22:53:23 -0400

    Initialize `pry` console

---
+++ config/initializers/pry.rb
@@ -0,0 +1,13 @@
+if defined?(Pry)
+  color_start = color_end = ''
+
+  if Pry.config.color
+    # Red for Production, Green otherwise.
+    color_start = Rails.env.production? ? "\033[31m" : "\033[32m"
+    color_end   = "\033[0m"
+  end
+
+  application = "\033[33m" + Rails.application.class.name.split("::").first + "\033[0m"
+
+  Pry.config.prompt_name = "pry|#{application}|#{color_start}#{Rails.env}#{color_end}"
+end
