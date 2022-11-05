0224583d05a9b5dd295a2a02519e156d736f47d7
abhchand <me@abhchand.me>
2022-11-05 10:50:59 -0400

    Increase Capybara timeout to 5 seconds

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -6,6 +6,7 @@ require 'capybara-screenshot/rspec'
 Capybara.configure do |config|
   config.ignore_hidden_elements = true
   config.javascript_driver = :headless_chrome
+  config.default_max_wait_time = 5
 end
 
 Capybara::Screenshot.autosave_on_failure = false
