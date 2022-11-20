cf7ce4c42540569b927d1a4b833526205d5553c2
abhchand <me@abhchand.me>
2022-11-20 01:12:03 -0500

    Allow displaying `<ModalError>` inside form block

--- app/frontend/javascript/components/modal_form/block.jsx
+++ app/frontend/javascript/components/modal_form/block.jsx
@@ -1,20 +1,25 @@
+import ModalError from 'javascript/components/modal/error';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 const Block = (props) => {
+
   return (
-    <div className='modal-form__block'>
+    <div className='modal-form__block' data-for={props.labelFor}>
       <label htmlFor={props.labelFor}>{props.label}</label>
 
       <div className='modal-form__block-element'>
         {props.children}
       </div>
+
+      {props.errorText && <ModalError text={props.errorText} />}
     </div>
   );
 };
 
 Block.propTypes = {
   children: PropTypes.node,
+  errorText: PropTypes.string,
   label: PropTypes.string.isRequired,
   labelFor: PropTypes.string.isRequired
 };
