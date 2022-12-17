8bdc1ac9d232b7e7e41591fd48a4b147530a51f1
abhchand <me@abhchand.me>
2022-12-17 15:06:21 -0500

    Add `textarea`-specific helpers and update logic to filter on element type

--- spec/frontend/javascript/support/form-helpers.js
+++ spec/frontend/javascript/support/form-helpers.js
@@ -4,11 +4,18 @@
  */
 
 const getFormInputValue = (wrapper, css) => {
-  return wrapper.find(css).at(0).prop('value');
+  /*
+   * Typically `find()` should be sufficient (especially for an `id` which is
+   * supposed to be unique). But some libraries nest elements with the same
+   * attributes, so we need to filter by element type too
+   */
+  return(
+    wrapper.find(css).filterWhere((n) => n.type() === 'input').at(0).prop('value')
+  );
 }
 
 const setFormInputValue = (wrapper, css, text) => {
-  const input = wrapper.find(css).at(0);
+  const input = wrapper.find(css).filterWhere((n) => n.type() === 'input').at(0);
 
   // Both are required, to set the value and trigger the handler
   input.getDOMNode().value = text;
@@ -38,9 +45,34 @@ const setFormSelectId = (wrapper, css, id) => {
   });
 }
 
+/*
+ * Textarea Fields
+ */
+
+const getFormTextareaValue = (wrapper, css) => {
+  /*
+   * Typically `find()` should be sufficient (especially for an `id` which is
+   * supposed to be unique). But some libraries nest elements with the same
+   * attributes, so we need to filter by element type too
+   */
+  return(
+    wrapper.find(css).filterWhere((n) => n.type() === 'textarea').at(0).prop('value')
+  );
+}
+
+const setFormTextareaValue = (wrapper, css, text) => {
+  const textarea = wrapper.find(css).filterWhere((n) => n.type() === 'textarea').at(0);
+
+  // Both are required, to set the value and trigger the handler
+  textarea.getDOMNode().value = text;
+  textarea.simulate('change', { currentTarget: { value: text } });
+}
+
 export {
   getFormInputValue,
   setFormInputValue,
   getFormSelectId,
-  setFormSelectId
+  setFormSelectId,
+  getFormTextareaValue,
+  setFormTextareaValue
 }
