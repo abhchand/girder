e882212e753abc8f226f0d1b3ed535092908ddc9
abhchand <me@abhchand.me>
2022-12-18 18:51:26 -0500

    Add `CtaButton` to UI Kit

---
+++ app/frontend/javascript/components/ui-kit/buttons/cta.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const CtaButton = (props) => {
+  return (
+    <input
+      type={'button'}
+      className={`cta ${props.additionalClasses.join(' ')}`}
+      value={props.value}
+      disabled={props.disabled}
+      onClick={props.onClick}
+    />
+  );
+};
+
+CtaButton.propTypes = {
+  additionalClasses: PropTypes.arrayOf(PropTypes.string),
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired
+};
+
+CtaButton.defaultProps = {
+  additionalClasses: [],
+  disabled: false
+}
+
+export default CtaButton;
