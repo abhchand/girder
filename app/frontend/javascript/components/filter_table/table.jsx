0cef79e23ea8fb438682523c2ae99272ca617895
abhchand <me@abhchand.me>
2022-12-19 17:22:58 -0500

    Add clarifying comment about empty state terminology

--- app/frontend/javascript/components/filter_table/table.jsx
+++ app/frontend/javascript/components/filter_table/table.jsx
@@ -2,6 +2,14 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 const Table = (props) => {
+  /*
+   * This is *not* a component-level empty state. It is a table-level empty state
+   * that's shown when there are 0 displayable items (because of a search filter
+   * or any other reason).
+   *
+   * If the FilterTable itself should be replaced with alternate when zero rows
+   * exist, that will need to be implemented outside the <FilterTable>.
+   */
   if (props.itemCount === 0) {
     const i18nPrefix = 'components.filter_table.table';
     return (
