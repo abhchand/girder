2fc5f2acfd739d8344cf3130ebb9218293c40c95
abhchand <me@abhchand.me>
2022-12-18 22:14:09 -0500

    Add `LinkButton` to UI Kit

---
+++ app/frontend/javascript/components/ui-kit/buttons/link.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const LinkButton = (props) => {
+  return (
+    <button
+      type='button'
+      className={`link-btn ${props.additionalClasses.join(' ')}`}
+      onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+LinkButton.propTypes = {
+  additionalClasses: PropTypes.arrayOf(PropTypes.string),
+  children: PropTypes.node,
+  onClick: PropTypes.func.isRequired
+};
+
+LinkButton.defaultProps = {
+  additionalClasses: [],
+}
+
+export default LinkButton;
