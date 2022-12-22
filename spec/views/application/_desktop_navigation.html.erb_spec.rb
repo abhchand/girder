19865caf312bd2cf9dbec201c1c9fbcfb2e5290c
abhchand <me@abhchand.me>
2022-12-22 13:09:14 -0500

    Reduce scope of navigation view tests

--- spec/views/application/_desktop_navigation.html.erb_spec.rb
+++ spec/views/application/_desktop_navigation.html.erb_spec.rb
@@ -8,29 +8,6 @@ RSpec.describe 'application/_desktop_navigation.html.erb', type: :view do
     forward_can_method_to(user)
   end
 
-  it 'renders the logo' do
-    render
-
-    logo = page.find('.desktop-navigation__logo')
-
-    expect(logo.find('a')['href']).to eq(root_path)
-    expect(logo).to have_selector('svg')
-  end
-
-  it 'renders the navigation links' do
-    render
-
-    actual_links = rendered_links
-    expected_links = [
-      photos_path,
-      account_profile_index_path,
-      '#',
-      destroy_user_session_path
-    ]
-
-    expect(expected_links).to eq(actual_links)
-  end
-
   context 'user can access admin pages' do
     before { user.add_role(:admin) }
 
--- spec/views/application/_mobile_navigation.html.erb_spec.rb
+++ spec/views/application/_mobile_navigation.html.erb_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'application/_mobile_navigation.html.erb', type: :view do
   let(:user) { create(:user) }
-  let(:user_presenter) { UserPresenter.new(user, view: view_context) }
 
   before do
     stub_view_context
@@ -10,41 +9,6 @@ RSpec.describe 'application/_mobile_navigation.html.erb', type: :view do
     forward_can_method_to(user)
 
     @t_prefix = 'application.mobile_navigation'
-  end
-
-  it "displays the user's profile" do
-    render
-
-    profile = page.find('.mobile-navigation__profile')
-
-    # Profile Picture
-    expect(profile.find('a')['href']).to eq(root_path)
-    expect(profile.find('img')['src']).to eq(
-      user_presenter.avatar_path(size: :thumb)
-    )
-
-    # User Name
-    expect(profile.find('span')).to have_content(user.first_name.downcase)
-  end
-
-  it 'renders the navigation links' do
-    render
-
-    actual_links = rendered_links
-    expected_links = [
-      photos_path,
-      account_profile_index_path,
-      '#',
-      destroy_user_session_path
-    ]
-
-    expect(expected_links).to eq(actual_links)
-  end
-
-  it 'renders the close link' do
-    render
-
-    expect(page).to have_selector('.mobile-navigation__close > svg')
   end
 
   context 'user can access admin pages' do
