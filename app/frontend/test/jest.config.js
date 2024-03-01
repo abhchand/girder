909a15b071af6561c95760b7888e73273c6c34f3
abhchand <me@abhchand.me>
2024-03-01 00:39:20 -0500

    mock CSS imports in Jest
    See: https://stackoverflow.com/a/54646930/2490003

--- app/frontend/test/jest.config.js
+++ app/frontend/test/jest.config.js
@@ -4,6 +4,11 @@ module.exports = {
   roots: ['app/frontend/src', 'app/frontend/test'],
   moduleFileExtensions: ['js', 'jsx'],
   moduleDirectories: ['node_modules', 'app/frontend'],
+  moduleNameMapper: {
+    // Jest can't handle CSS imports, so we need to mock it
+    // See: https://stackoverflow.com/a/54646930/2490003
+    '\\.(css|scss)$': '<rootDir>/app/frontend/test/mocks/styleMock.js',
+  },
   setupFilesAfterEnv: ['<rootDir>/app/frontend/test/jest.setup.js'],
   transform: {
     '\\.jsx?$': 'babel-jest'
---
+++ app/frontend/test/mocks/styleMock.js
@@ -0,0 +1,5 @@
+/*
+ * A placeholder mock to stand in for CSS style imports
+ * See: https://stackoverflow.com/a/54646930/2490003
+ */
+module.exports = {};
